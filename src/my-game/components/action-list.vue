9632ec88e49f0c1236ccaf5a289396d673b8d249
Classified3939 <classified390@gmail.com>
2022-12-06 08:09:42 -0700

    Add vueFilters, change title and icon

--- src/my-game/components/action-list.vue
+++ src/my-game/components/action-list.vue
@@ -1,27 +1,32 @@
 <template>
-    <igt-feature style="height:45rem; width:20rem;" containerClass="justify-between bg-gray-200 dark:bg-gray-700">
-        Actions
-        <div v-for="actionFeature in actionList" :key="actionFeature.skillAction.description">
-            <csp-action :actionFeature="actionFeature"></csp-action>
-        </div>
-    </igt-feature>
+  <igt-feature
+    style="height:45rem; width:20rem;"
+    containerClass="justify-between bg-gray-200 dark:bg-gray-700"
+  >
+    Actions
+    <div
+      v-for="actionFeature in actionList"
+      :key="actionFeature.skillAction.description"
+    >
+      <csp-action :actionFeature="actionFeature"></csp-action>
+    </div>
+  </igt-feature>
 </template>
 
 <script>
-import {App} from "@/App.ts";
-import IgtFeature from "@/components/util/igt-feature"
+import { App } from "@/App.ts";
+import IgtFeature from "@/components/util/igt-feature";
 import CspAction from "@/my-game/components/csp-action";
 
-export default{
-    name: "action-test",
-    components: {CspAction, IgtFeature},
-    data(){
-        return{
-            actionList: App.game.features.actionList.actions
-        }
-    },
-}
+export default {
+  name: "action-list",
+  components: { CspAction, IgtFeature },
+  data() {
+    return {
+      actionList: App.game.features.actionList.actions,
+    };
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
